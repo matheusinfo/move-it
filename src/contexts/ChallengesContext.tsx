824e2c4d7aed6730168da863eb591a4fde748c2c
MatheusINFO <matheus.add12@gmail.com>
2021-02-27 09:45:11 -0300

    feat: add context

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, ReactNode, useState } from 'react'
+
+type ChallengesProviderProps = {
+    children: ReactNode
+}
+
+type ChallengesContextData = {
+    level: number
+    currentExperience: number
+    challengesCompleted: number
+    levelUp: () => void
+    startNewChallenge: () => void
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData)
+
+export function ChallengesProvider ({ children }: ChallengesProviderProps){
+    const [level, setLevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [challengesCompleted, setChallengesCompleted] = useState(0)
+
+    function levelUp(){
+        setLevel(level+1)
+    }
+
+    function startNewChallenge(){
+        console.log('New Challenge')
+    }
+
+    return(
+        <ChallengesContext.Provider 
+            value={{
+                level, 
+                currentExperience, 
+                challengesCompleted, 
+                levelUp,
+                startNewChallenge
+            }}
+        >
+            {children}
+        </ChallengesContext.Provider>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/color.css'
 import GlobalStyle from '../styles/global'
 
+import { ChallengesProvider } from '../contexts/ChallengesContext'
+
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ChallengesProvider>
       <GlobalStyle/>
       <Component {...pageProps} />
-    </>
+    </ChallengesProvider>
   )
 }
 
